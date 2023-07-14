7a556dbecf62e35b6c71d0c39ca222facdf9bfd8
nellBluff <nailiavagizova@gmail.com>
2023-07-14 16:18:45 +0300

    Pretty

--- runner.js
+++ runner.js
@@ -1,10 +1,10 @@
 // Подключить модуль работы с файловой системой.
-const fs = require("fs");
+const fs = require('fs');
 
 // Подключить функцию readAndSolve из соответствующего файла.
-const readAndSolve = require("./readAndSolve");
+const readAndSolve = require('./readAndSolve');
 
 // Все судоку для решения доступны в файле puzzles.txt.
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
-fs.readFile("./puzzles.txt", "utf-8", readAndSolve);
-console.log("hello");
+fs.readFile('./puzzles.txt', 'utf-8', readAndSolve);
+console.log('hello');
--- sudoku.js
+++ sudoku.js
@@ -1,31 +1,55 @@
+var Table = require('cli-table');
+var table = new Table({
+    head: ['^^', 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    chars: {
+        top: '═',
+        'top-mid': '╤',
+        'top-left': '╔',
+        'top-right': '╗',
+        bottom: '═',
+        'bottom-mid': '╧',
+        'bottom-left': '╚',
+        'bottom-right': '╝',
+        left: '║',
+        'left-mid': '╟',
+        mid: '─',
+        'mid-mid': '┼',
+        right: '║',
+        'right-mid': '╢',
+        middle: '│',
+    },
+});
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
-}
+function solve(boardString) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
 
+function isSolved() {}
+
+function prettyBoard(solvedArr) {
+    solvedArr.forEach((el, i) => {
+        table.push({ [i + 1]: el });
+    });
+
+    console.log(table.toString());
 }
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
+    solve,
+    isSolved,
+    prettyBoard,
 };
