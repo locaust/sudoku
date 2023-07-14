b7e918f6515a69875d5fa71355c1118feab076d5
Ekaterina Bakhmeteva <ekaterina.kukhorevich@gmail.com>
2023-07-14 16:00:11 +0300

    f solve aded

--- runner.js
+++ runner.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 // Подключить функцию readAndSolve из соответствующего файла.
 const readAndSolve = require("./readAndSolve");
+const { log } = require("console");
+const { EOL } = require("os");
 
 // Все судоку для решения доступны в файле puzzles.txt.
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
